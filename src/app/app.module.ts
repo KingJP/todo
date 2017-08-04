a72dff3337b2bfa889bb32ba792d332104babeff
Pylypiw <jochen@pylypiw.com>
2017-08-04 16:39:31 +0200

    Bootstrap CDN + Login Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TodoLoginComponent } from './login/todo-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoLoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: TodoLoginComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
